afbc3cf4a6efa33695b06d72ad002a12ae21fb52
hikkywannafly <ggraygon@gmail.com>
2025-06-19 20:52:53 +0700

    feat: skeleton

---
+++ src/components/shared/sekeleton.tsx
@@ -0,0 +1,42 @@
+import classNames from "classnames";
+import React from "react";
+
+export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+    className?: string;
+}
+
+const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
+    ({ className, ...props }, ref) => {
+        return (
+            <div ref={ref} className={classNames(className)} {...props}>
+                {props.children}
+            </div>
+        );
+    }
+);
+Skeleton.displayName = "Skeleton";
+
+interface skeletonItemProps extends React.HTMLProps<HTMLDivElement> {
+    className?: string,
+    container?: boolean,
+}
+
+export const SkeletonItem: React.FC<skeletonItemProps> = ({
+    container,
+    className,
+    ...props
+
+}) => {
+    return (
+        <div
+            className={classNames(
+                container && "animate-pulse bg-white/20",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+
+
+export default Skeleton;
