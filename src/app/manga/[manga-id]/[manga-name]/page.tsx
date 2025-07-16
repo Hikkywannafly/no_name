e656960262a6451458ed9eb9cd1fc0f94a5d8196
hikkywannafly <ggraygon@gmail.com>
2025-07-16 22:38:41 +0700

    fix: fix

--- src/app/manga/[manga-id]/[manga-name]/page.tsx
+++ src/app/manga/[manga-id]/[manga-name]/page.tsx
@@ -1,29 +1,26 @@
-import BaseLayout from "@/app/baseLayout";
-import { Manga } from "@/components/manga/manga";
-import { getMediaDetails } from "@/provider/Anilist";
-import { MediaType } from "@/types/anilist";
+import BaseLayout from "@/app/baseLayout"
+import { Manga } from "@/components/manga/manga"
+import { getMediaDetails } from "@/provider/Anilist"
+import { MediaType } from "@/types/anilist"
 
 export default async function MangaPage({
   params,
 }: {
-  params: { "manga-id": string; "manga-name": string };
+  params: Promise<{ "manga-id": string; "manga-name": string }>
 }) {
-  const mangaId = Number(params["manga-id"]);
-  const mangaName = params["manga-name"];
+
+  const { "manga-id": mangaIdParam, "manga-name": mangaName } = await params
+
+  const mangaId = Number(mangaIdParam)
 
   const mangaData = await getMediaDetails({
     id: mangaId,
     type: MediaType.Manga,
-  });
+  })
 
   return (
     <BaseLayout showHeader={true} showFooter={true}>
-      <Manga
-
-        mangaId={mangaId}
-        name={mangaName}
-        prefetchManga={mangaData}
-      />
+      <Manga mangaId={mangaId} name={mangaName} prefetchManga={mangaData} />
     </BaseLayout>
-  );
+  )
 }
